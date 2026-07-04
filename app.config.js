7a1be0b5bfa5f5f66f635009e0a6f2afe609c64a
Raouf Abbou <53510613+aabb1998@users.noreply.github.com>
2026-07-04 11:41:14 +1000

    Bump app version to 1.0.6 and update Stripe key
    Bump application version to 1.0.6 and increment iOS build number to 6 for release. Updated version strings in app.config.js, app.json, and package.json, and updated CURRENT_PROJECT_VERSION / MARKETING_VERSION in the iOS Xcode project. Also replaced EXPO_PUBLIC_STRIPE_KEY in eas.json (for both internal and autoIncrement profiles) with a new public key used by EAS builds.

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "Al-Ihsan Zakat & Charity App",
     slug: "alihsan-mobile",
     owner: "alihsan2025s-organization",
-    version: "1.0.5",
+    version: "1.0.6",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     userInterfaceStyle: "light",
@@ -20,7 +20,7 @@ module.exports = {
     },
     assetBundlePatterns: ["**/*"],
     ios: {
-      buildNumber: "5",
+      buildNumber: "6",
       supportsTablet: true,
       bundleIdentifier: "org.alihsan.mobile",
       usesAppleSignIn: true,
